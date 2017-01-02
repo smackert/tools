2bf9d6fb0fc7faf331c75540d42dba997bfa0bb3
Sean Mackert <smackert@users.noreply.github.com>
2017-01-02 00:29:07 -0500

    Initial commit

---
+++ ipscan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Slow single thread scan of the /24
+import os
+
+addr = []
+for i in range(0,256):
+	ip = "10.11.1.%s" % i
+	result = os.system("ping -c 1 %s" % ip)
+	if (result == 0):
+		addr.append(ip)
+
+print("Live hosts:")
+for i in addr:
+	print i + "\r"
